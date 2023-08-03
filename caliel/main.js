0c7da4f6df9c0758f2424f43f9aafbdbdfc3f623
djcalieldocruzeiro <139808773+djcalieldocruzeiro@users.noreply.github.com>
2023-08-03 16:28:58 -0300

    Add files via upload

---
+++ caliel/main.js
@@ -0,0 +1,14 @@
+document.getElementByld("btn-laranja")
+.addEventListener("click",function()
+{
+    document.querySelector("style").setAttribute("class","laranja");
+})
+
+document.getElementByld("btn-roxo").addEventListener("click",function()
+{
+    document.querySelector("style").setAttribute("class","roxo");
+})
+document.getElementByld("btn-verde").addEventListener("click",function()
+{
+    document.querySelector("style").setAttribute("class","verde");
+})
